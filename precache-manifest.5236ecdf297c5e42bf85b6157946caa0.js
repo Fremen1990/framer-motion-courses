e6c8683d974d80bdf5104b6ebec9a59864ad2156
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-13 23:16:22 +0100

    Updates

---
+++ precache-manifest.5236ecdf297c5e42bf85b6157946caa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a180312fee293de8b8647775c465758",
+    "url": "/framer-motion-courses/index.html"
+  },
+  {
+    "revision": "e21836bf3393b46353ed",
+    "url": "/framer-motion-courses/static/css/main.a9b466e9.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/framer-motion-courses/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion-courses/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21836bf3393b46353ed",
+    "url": "/framer-motion-courses/static/js/main.9d175059.chunk.js"
+  },
+  {
+    "revision": "39d41209fe28b7cb45df",
+    "url": "/framer-motion-courses/static/js/runtime-main.88b6249e.js"
+  }
+]);
